868b66a7227892aa19fb34d4b7afcde760780e3a
Alekbs <aleksandrb436@gmail.com>
2022-12-29 23:22:19 +0300

    Добавил синтез речи и функцию time_now

--- Aud/main.py
+++ Aud/main.py
@@ -1,52 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import pyaudio
-from vosk import Model, KaldiRecognizer
-from flask import Flask, Response, render_template, request
+
 import json
+import pyaudio
+import pyttsx3
 import time
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('alek.html')
+from datetime import datetime, date, time
+from vosk import Model, KaldiRecognizer
 
 
-@app.route("/process_audio")
-def process_audio():
-    url = request.args.get("url")
-    print("url = ", url)
-
-
-@app.route("/listen")
 def listen():
-    def respond_to_client():
-        while True:
-            stream.start_stream()
-            data = stream.read(4000, exception_on_overflow=False)
-            if len(data) == 0:
-                break
-            if (rec.AcceptWaveform(data)) and (len(data) > 0):
-                answer = json.loads(rec.Result())
-                text = answer['text']
-                print(text)
-                _data = json.dumps({"text": text})
-                yield f"id: 1\ndata: {_data}\nevent: online\n\n"
+    while True:
+        data = stream.read(4000, exception_on_overflow=False)
+        if (rec.AcceptWaveform(data)) and (len(data) > 0):
+            answer = json.loads(rec.Result())
+            if answer["text"]:
+                yield answer["text"]
 
-        time.sleep(0.001)
-    return Response(respond_to_client(), mimetype='text/event-stream')
+
+def say(msg):
+    print(msg)
+    engine.say(msg)
+    engine.runAndWait()
+
+
+def time_now():
+    time_check = datetime.now()
+    say(f"Время: {time_check.hour} часа {time_check.minute} минут")
+
+
+def main():
+
+    say("Я вас слушаю")
+    for text in listen():
+        print(text)
+        if "который час" in text:
+            time_now()
+        elif "выход" in text:
+            say("До свидания...")
+            exit()
 
 
 if __name__ == "__main__":
+    engine = pyttsx3.init()
+    ru_voice_id = "HKEY_LOCAL-MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0"
+    engine.setProperty("voice", ru_voice_id)
     model = Model("vosk-model-small-ru-0.4")
-    rec = KaldiRecognizer(model, 8000)
+    rec = KaldiRecognizer(model, 16000)
     p = pyaudio.PyAudio()
     stream = p.open(
         format=pyaudio.paInt16,
         channels=1,
-        rate=8000,
+        rate=16000,
         input=True,
-        frames_per_buffer=8000
+        frames_per_buffer=8000,
     )
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    stream.start_stream()
+    main()
